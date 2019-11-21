2b7b35cb0b84944f71a96bd6d35776dd0787e866
Jonathan Yale <jonathanyale@gmail.com>
2019-11-21 14:48:57 -0500

    Added video directory to .gitignore. All other changes

--- ex13/code.js
+++ ex13/code.js
@@ -1,6 +1,7 @@
 const geometry = require('./geometry');
 
-let area51 = geometry.area(2.8)
+let area51 = geometry.area(2.8);
 let circ2 = geometry.circumference(6);
+let earth = geometry.volume(3963);
 
-console.log(`Area: ${area51}, Circumference: ${circ2}`);
+console.log(`Area: ${area51},\nCircumference: ${circ2},\nVolume: ${earth}`);
--- ex13/geometry.js
+++ ex13/geometry.js
@@ -1,7 +1,9 @@
 const area = (r) => Math.PI * r ** 2;
 const circumference = (r) => 2 * Math.PI * r;
+const volume = (r) => 4 / 3 * Math.PI * r ** 3;
 
 module.exports = {
     area: area,
-    circumference: circumference
+    circumference: circumference,
+    volume: volume
 }
